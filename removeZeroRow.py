b83b7ba5d394108ed109c53f4dacecff1dcf522e
zhiyao <zhiyao@zhiyao-Pro.local>
2016-11-12 17:20:39 +0800

    remove zero line

--- removeZeroRow.py
+++ removeZeroRow.py
@@ -1,5 +1,14 @@
-fw = open('zunlaFpkmNoZero.csv', 'w')
-for line in open('zunlaFpkm.csv', 'r'):
+import sys
+
+if len(sys.argv) != 2:
+    print "Need exactly one command line argument!"
+
+infile = sys.argv[1]
+outfile = sys.argv[1].replace('.csv', '') + 'NoZero.csv'
+print outfile
+
+fw = open(outfile, 'w')
+for line in open(infile, 'r'):
     isZero = True
     lineS = line.split(',')
     del lineS[0] 
@@ -12,6 +21,6 @@ for line in open('zunlaFpkm.csv', 'r'):
 
     if isZero == False:
         fw.write(line)
-    else:
-        print line
+#    else:
+        #print line
 
